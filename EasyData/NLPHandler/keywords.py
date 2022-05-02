70998006e2569e96d92db9c88ae6011f063b77f6
Longhu Qin <1097824882@qq.com>
2022-05-02 14:37:45 +0800

    [Feat] gensim tfidf

--- EasyData/NLPHandler/keywords.py
+++ EasyData/NLPHandler/keywords.py
@@ -1,6 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import jieba
+import re
+import string
+from gensim.models import TfidfModel
+from gensim.corpora import Dictionary
+
 
 def get_tfdif_keywords(text_list, top_num=10):
   _tfidf = TfidfVectorizer(stop_words=None, binary=True, max_features=25_000) # stop_words='english'
@@ -9,9 +14,7 @@ def get_tfdif_keywords(text_list, top_num=10):
   print("len of features",len(_tfidf.get_feature_names()))
   mm = np.mean( text_embeddings, axis=0 )
   ii = np.argsort(mm)[-top_num:][::-1].tolist()
-
   top_words = [(_tfidf.get_feature_names()[i], mm[i]) for i in ii]
-
   print(f"{top_num} top words: \n",*top_words[:10], " ...")
   return top_words
 
@@ -20,6 +23,41 @@ def get_keyword(text):
     topk = min(3,len(text))
     keyword = [word for word in jieba.analyse.textrank(text, topK = topk)]
     return keyword
+
+
+class gensimTfidf(object):
+    def __init__(self, items):
+        
+        self.dct = Dictionary(items)  # fit dictionary
+        
+        # self.dct.filter_tokens(bad_ids=[self.dct.token2id['ema']])
+
+        self.corpus = [self.dct.doc2bow(item) for item in items]  # convert corpus to BoW format
+        self.model = TfidfModel(self.corpus)  # fit model
+
+    def get_item_top(self, item, top_k=10, filter_key=None):
+        bow_item  = self.dct.doc2bow(item)
+        tfidf_pairs = self.model[bow_item]  # apply model to the first corpus document
+        
+        tfidf_pairs = sorted(tfidf_pairs, key=lambda x: x[1], reverse=True)
+
+        top_k = min(top_k, int(len(bow_item)/2) )
+
+        keywords = [self.dct.get(k) for k,v in tfidf_pairs[:top_k]]
+        if filter_key:
+            keywords =  [k for k in keywords if not filter_key(k)]
+        
+        return keywords
+
+    def get_corpus_top(self, top_k=10):
+        return self.dct.filter_n_most_frequent(top_k)
+
+
+def filter_num(str):
+    if str in string.punctuation:
+        return True
+    pattern = re.compile(r"\d*\(\d+/\d+\)\d*|\d+\.\d+%?|\d+%?")
+    return re.match(pattern, str)
 
 
   
