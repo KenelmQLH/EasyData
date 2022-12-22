1d380415275d1e2165cc68d9a76acf6323ba7abb
qlh <qlonghu@mail.ustc.edu.cn>
2022-12-22 08:41:55 +0000

    Update equation_mode

--- EasyData/NLPHandler/Math/common.py
+++ EasyData/NLPHandler/Math/common.py
@@ -1,12 +1,14 @@
 import re
 COMMON_NUM_PATTERN = r"(\d+\.\d+%?|\d+%?)"
 
-TEXT_NUM_PATTERN = r"(\d+(,\d\d\d)+|\d*\(\d+/\d+\)\d*|\d*\.\d+%?|\d+%?)"
+# TEXT_NUM_PATTERN = r"(\d+(,\d\d\d)+|\d*\(\d+/\d+\)\d*|\d*\.\d+%?|\d+%?)"
+TEXT_NUM_PATTERN = r"(\d+(,\d\d\d)+|\d+\.\d+%?|\d+%?|\d*\(\d+/\d+\)\d*)"
 """
     Patten1: 1,222,333
-    Pattte2: 2(1/3)4
-    Patten3: 1.2 或 1.2% 或 .2 或 .2%
+    Patten3: 1.2 或 1.2% （丢用：或 .2 或 .2%)
     Patten4: 12 或 12%
+    Pattte2: 2(1/3)4
+
 """
 
 def is_math_num(number: str, pattern_str=COMMON_NUM_PATTERN):
